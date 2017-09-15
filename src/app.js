7c05314911640aa3bc539e512a96738d8d6eda30
Sean Chen <skypemaster007@gmail.com>
2017-09-14 19:03:04 -0700

    Moving all files from private Auth repo to public Auth repo

---
+++ src/app.js
@@ -0,0 +1,8 @@
+// Do NOT modify this file; make your changes in server.js.
+const { server } = require('./server.js');
+const PORT = 5000;
+
+server.listen(PORT, () => {
+    console.log(`Server listening on post ${PORT}`);
+});
+
